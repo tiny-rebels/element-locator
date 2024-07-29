024d6877422b31c14ca752bbeb5f19a7273810b2
Stefan Korfitz <stefan@korfitz.com>
2024-07-29 20:38:31 +0200

    Started a new project on github

---
+++ src/Lookup.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Element\Locator;
+
+class Lookup {
+
+
+    public function __construct() {
+
+    }
+
+    /**
+     * @return array|false|string
+     */
+    public static function ipAddress() {
+
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+
+        else if(getenv('HTTP_FORWARDED'))
+            $ipaddress = getenv('HTTP_FORWARDED');
+
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+
+        else
+            $ipaddress = 'UNKNOWN';
+
+        return $ipaddress;
+    }
+}
